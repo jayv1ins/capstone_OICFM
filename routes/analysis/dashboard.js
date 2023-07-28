82690fca5c2e8676ffbefb6d97313c24bd6788ad
jayv1ins <nakpil47@gmail.com>
2023-07-28 15:40:57 +0800

    Dashboard Filter date

--- routes/analysis/dashboard.js
+++ routes/analysis/dashboard.js
@@ -2,6 +2,5 @@ const router = require('express').Router();
 const dashboardController = require('../../controllers/analysis/dashboardController');
 
 router.get('/dashboard', dashboardController.getDashboard);
-router.get("/api/filterData", dashboardController.filterData);
 
 module.exports = router;
